64091fd70fcdcd63f3d9491b589092356c90522d
Amruth <amruthlp12@gmail.com>
2025-08-12 19:46:18 +0530

    Remove Profile model and associated views, templates, and URLs

--- pdf_app/models.py
+++ pdf_app/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
-class Profile(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    age = models.IntegerField()
-    bio = models.TextField()
 
-    def __str__(self):
-        return self.name
